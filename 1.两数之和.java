4d82dee0463c4368fe4794b9adc6c79dc20df15a
vimday <lwfplus@gmail.com>
2020-02-04 23:06:06 +0800

    添加常用代码

---
+++ 1.两数之和.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int [] res=new int[2];
+        
+    }
+}
+// @lc code=end
+
