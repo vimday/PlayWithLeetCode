d7eaca44206d88c63d737968d795ca62beee2645
vimday <lwfplus@gmail.com>
2020-02-23 23:03:00 +0800

    add some other methods

--- BiweeklyContest-20(Rank89)/A-sort-integers-by-the-number-of-1-bits.cpp
+++ BiweeklyContest-20(Rank89)/A-sort-integers-by-the-number-of-1-bits.cpp
@@ -27,3 +27,23 @@ public:
         return arr;
     }
 };
+class Solution {
+public:
+    vector<int> sortByBits(vector<int>& a) {
+        vector <int> ret;
+        vector <pair <int, int> > v;
+        for (int i = 0; i < a.size(); i++) {
+            int s = 0, cur = a[i];
+            while (cur > 0) {
+                s += (cur % 2);
+                cur /= 2;
+            }
+            v.push_back(make_pair(s, a[i]));
+        }
+        sort(v.begin(), v.end());
+        for (int i = 0; i < v.size(); i++) {
+            ret.push_back(v[i].second);
+        }
+        return ret;
+    }
+};
--- BiweeklyContest-20(Rank89)/C-number-of-substrings-containing-all-three-characters.cpp
+++ BiweeklyContest-20(Rank89)/C-number-of-substrings-containing-all-three-characters.cpp
@@ -49,3 +49,47 @@ public:
 
     }
 };
+//双指针解法
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n = s.size();
+        int cnt[5];
+        cnt[0] = cnt[1] = cnt[2] = 0;
+        int r = -1;
+        int ans = 0;
+        for (int l = 0; l < n; l++) {
+            while ((cnt[0] == 0 || cnt[1] == 0 || cnt[2] == 0) && r < n - 1) {
+                r++;
+                cnt[s[r] - 'a']++;
+            }
+            //printf("%d %d\n", l, r);
+            if (cnt[0] > 0 && cnt[1] > 0 && cnt[2] > 0) {
+                ans += (n - 1 - r + 1);
+            }
+            cnt[s[l] - 'a']--;
+        }
+        return ans;
+    }
+};
+//第一种方法的优化改良版 tql
+// class Solution {
+//     public int numberOfSubstrings(String s) {
+//         int res = 0;
+//         // 记录 'a', 'b', 'c' 上次出现的位置
+//         int[] record = {-1, -1, -1}; 
+//         for (int i = 0; i < s.length(); i++) {
+//             if (s.charAt(i) == 'a') {
+//                 res += Math.min(record[1], record[2]) + 1;
+//             } else if (s.charAt(i) == 'b') {
+//                 res += Math.min(record[0], record[2]) + 1;
+//             } else {
+//                 res += Math.min(record[0], record[1]) + 1;
+//             }
+//             // 更新 'a', 'b', 'c' 最近出现的位置
+//             record[s.charAt(i) - 'a'] = i;
+//         }
+//         return res;
+//     }
+// }
+// 以索引 i 遍历字符串 s ，考虑子串 (k, i) 满足条件的最大 k，那么此时子串(0, i), ...(k-1, i) 也是符合条件的子串
