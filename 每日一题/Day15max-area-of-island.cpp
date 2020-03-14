29017a75711040ee9207ecf61926569abaf8f83f
vimday <lwfplus@gmail.com>
2020-03-15 00:33:35 +0800

    use google format

--- 每日一题/Day15max-area-of-island.cpp
+++ 每日一题/Day15max-area-of-island.cpp
@@ -9,40 +9,32 @@
  */
 #include <bits/stdc++.h>
 using namespace std;
-void debug()
-{
+void debug() {
 #ifdef LOCAL
-    freopen("E:\\Cpp\\in.txt", "r", stdin);
-    freopen("E:\\Cpp\\out.txt", "w", stdout);
+  freopen("E:\\Cpp\\in.txt", "r", stdin);
+  freopen("E:\\Cpp\\out.txt", "w", stdout);
 #endif
 }
-class Solution
-{
-public:
-    int m, n, d[5] = {-1, 0, 1, 0, -1};
-    int maxAreaOfIsland(vector<vector<int>> &grid)
-    {
-        m = grid.size();
-        if (m == 0)
-            return 0;
-        n = grid[0].size();
-        int res = 0;
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                if (grid[i][j])
-                    res = max(res, dfs(grid, i, j));
-        return res;
+class Solution {
+ public:
+  int m, n, d[5] = {-1, 0, 1, 0, -1};
+  int maxAreaOfIsland(vector<vector<int>> &grid) {
+    m = grid.size();
+    if (m == 0) return 0;
+    n = grid[0].size();
+    int res = 0;
+    for (int i = 0; i < m; i++)
+      for (int j = 0; j < n; j++)
+        if (grid[i][j]) res = max(res, dfs(grid, i, j));
+    return res;
+  }
+  int dfs(vector<vector<int>> &g, int i, int j) {
+    g[i][j] = 0;
+    int cs = 1;
+    for (int k = 0; k < 4; k++) {
+      int r = i + d[k], c = j + d[k + 1];
+      if (r >= 0 && r < m && c >= 0 && c < n && g[r][c]) cs += dfs(g, r, c);
     }
-    int dfs(vector<vector<int>> &g, int i, int j)
-    {
-        g[i][j] = 0;
-        int cs = 1;
-        for (int k = 0; k < 4; k++)
-        {
-            int r = i + d[k], c = j + d[k + 1];
-            if (r >= 0 && r < m && c >= 0 && c < n && g[r][c])
-                cs += dfs(g, r, c);
-        }
-        return cs;
-    }
+    return cs;
+  }
 };
