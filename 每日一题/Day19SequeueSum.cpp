c03e94a49418deaece42a0faf3a62b5c2641e4e7
vimday <lwfplus@gmail.com>
2020-03-19 13:40:42 +0800

    add day19

--- 每日一题/Day19SequeueSum.cpp
+++ 每日一题/Day19SequeueSum.cpp
@@ -15,3 +15,18 @@ void debug() {
     freopen("E:\\Cpp\\out.txt", "w", stdout);
 #endif
 }
+int main() {
+    int N, L;
+    cin >> N >> L;
+    for (int i = L; i <= 100; i++) {
+        if ((2 * N + i - i * i) % (2 * i) == 0) {
+            int start = (2 * N + i - i * i) / (2 * i);
+            for (int j = 0; j < i - 1; j++) {
+                cout << start + j << " ";
+            }
+            cout << start + i - 1;
+            return 0;
+        }
+    }
+    cout << "No" << endl;
+}
