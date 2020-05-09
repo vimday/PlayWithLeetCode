a53905add28b4c60758215f8e35758a38d9a860a
vimday <lwfplus@gmail.com>
2020-05-10 00:12:47 +0800

    add lc day71

---
+++ 每日一题/Day71lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,33 @@
+/*
+ * @Author      :vimday
+ * @Desc        :
+ * @Url         :
+ * @File Name   :Day71lowest-common-ancestor-of-a-binary-tree.cpp
+ * @Created Time:2020-05-10 00:10:05
+ * @E-mail      :lwftx@outlook.com
+ * @GitHub      :https://github.com/vimday
+ */
+#include <bits/stdc++.h>
+using namespace std;
+void debug() {
+#ifdef LOCAL
+    freopen("E:\\Cpp\\in.txt", "r", stdin);
+    freopen("E:\\Cpp\\out.txt", "w", stdout);
+#endif
+}
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || p == root || q == root) return root;
+        TreeNode* l = lowestCommonAncestor(root->left, p, q);
+        TreeNode* r = lowestCommonAncestor(root->right, p, q);
+        return !l ? r : !r ? l : root;
+    }
+};
