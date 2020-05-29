ca3888cba23bef76ce84806ee631f275eb13f4c0
vimday <lwfplus@gmail.com>
2020-05-30 00:48:43 +0800

    add lc day91 hard 柱状图中的最大矩形

--- 每日一题/Day91largest-rectangle-in-histogram.cpp
+++ 每日一题/Day91largest-rectangle-in-histogram.cpp
@@ -45,3 +45,25 @@ public:
         return res;
     }
 };
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& hts) {
+        //单调栈 递增栈
+        hts.push_back(0);
+        stack<int> s;
+        int n = hts.size(), res = 0;
+        for (int i = 0; i < n; i++) {
+            //最大值肯定是以摸个矩形的高度为最大高度,
+            //长度为其左右第一个小于其高度的索引之差相乘得出
+            //故只需要枚举每个索引即可，递增的话就入栈，出现减少则就可以枚举栈顶元素了！
+            while (!s.empty() && hts[s.top()] > hts[i]) {
+                int cur = s.top();
+                s.pop();
+                int pre = s.empty() ? -1 : s.top(), h = hts[cur];  //左边第一个小于 cur 的 idx，右边的为 i
+                res = max(res, (i - pre - 1) * h);
+            }
+            s.push(i);
+        }
+        return res;
+    }
+};
