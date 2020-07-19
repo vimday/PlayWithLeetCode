be29bf11b583a763955f46e461f6a829bace9a83
vimday <lwfplus@gmail.com>
2020-07-20 00:06:47 +0800

    add lc167 two-sum

---
+++ 每日一题 6-8 月/Day0720_two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,33 @@
+/*
+ * @Author      :vimday
+ * @Desc        :
+ * @Url         :
+ * @File Name   :Day0720_two-sum-ii-input-array-is-sorted.cpp
+ * @Created Time:2020-07-20 00:05:38
+ * @E-mail      :lwftx@outlook.com
+ * @GitHub      :https://github.com/vimday
+ */
+#include <bits/stdc++.h>
+using namespace std;
+void debug() {
+#ifdef LOCAL
+    freopen("E:\\Cpp\\in.txt", "r", stdin);
+    freopen("E:\\Cpp\\out.txt", "w", stdout);
+#endif
+}
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int low = 0, high = numbers.size() - 1;
+        while (low < high) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target)
+                return {low + 1, high + 1};
+            else if (sum < target)
+                ++low;
+            else
+                --high;
+        }
+        return {-1, -1};
+    }
+};
