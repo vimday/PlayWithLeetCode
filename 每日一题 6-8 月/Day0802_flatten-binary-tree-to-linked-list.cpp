2ece65429f52ef890321270347bcd24d783a3072
vimday <lwfplus@gmail.com>
2020-08-02 01:01:57 +0800

    add lc114 二叉树展开为链表

--- 每日一题 6-8 月/Day0802_flatten-binary-tree-to-linked-list.cpp
+++ 每日一题 6-8 月/Day0802_flatten-binary-tree-to-linked-list.cpp
@@ -48,3 +48,17 @@ public:
         lr->right = r;
     }
 };
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root)
+            return;
+        flatten(root->left);
+        flatten(root->right);
+        TreeNode* t = root->right;
+        root->right = root->left;
+        root->left = nullptr;
+        while (root->right) root = root->right;
+        root->right = t;
+    }
+};
