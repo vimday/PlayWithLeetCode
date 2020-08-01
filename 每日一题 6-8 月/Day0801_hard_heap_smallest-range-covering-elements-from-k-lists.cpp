c7323a64409281611a45bd0edb9a17122518eb45
vimday <lwfplus@gmail.com>
2020-08-02 00:37:33 +0800

    add lc114 二叉树展开为链表

--- 每日一题 6-8 月/Day0801_hard_heap_smallest-range-covering-elements-from-k-lists.cpp
+++ 每日一题 6-8 月/Day0801_hard_heap_smallest-range-covering-elements-from-k-lists.cpp
@@ -40,3 +40,80 @@ public:
         return res;
     }
 };
+
+struct NUM {
+    int val;
+    int pos;
+};
+struct INTERVAL_RST {
+    int start;
+    int end;
+};
+bool comparenum(NUM& num1, NUM& num2) {
+    if (num1.val < num2.val) {
+        return true;
+    }
+    return false;
+}
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        int size = nums.size();
+        vector<NUM> allNum;
+        NUM tmp = {0};
+        for (int i = 0; i < size; i++) {
+            int size_interv = nums[i].size();
+            for (int j = 0; j < size_interv; j++) {
+                tmp.val = nums[i][j];
+                tmp.pos = i;
+                allNum.push_back(tmp);
+            }
+        }
+        sort(allNum.begin(), allNum.end(), comparenum);
+        int numsize = allNum.size();
+        vector<int> IntervalFlag(size, 0);
+        int left = 0;
+        int right = 0;
+        int calculate = 0;
+        int mindif = allNum[numsize - 1].val - allNum[0].val;
+        int start = allNum[0].val;
+        int end = allNum[numsize - 1].val;
+        int starttmp = 0;
+        int endtmp = 0;
+        for (int i = 0; i < numsize; i++) {
+            int pos = allNum[i].pos;
+            if (IntervalFlag[pos] == 0) {
+                IntervalFlag[pos] = 1;
+                calculate++;
+            } else {
+                IntervalFlag[pos]++;
+            }
+
+            while (calculate == size) {
+                starttmp = allNum[left].val;
+                endtmp = allNum[i].val;
+                int dif = endtmp - starttmp;
+                if (dif < mindif) {
+                    start = starttmp;
+                    end = endtmp;
+                    mindif = dif;
+                } else if (dif == mindif) {
+                    if (starttmp < start) {
+                        start = starttmp;
+                        end = endtmp;
+                    }
+                }
+                //收缩左边
+                int pos2 = allNum[left].pos;
+                if (IntervalFlag[pos2] == 1) {
+                    calculate--;
+                }
+                IntervalFlag[pos2]--;
+                left++;
+            }
+        }
+
+        vector<int> Rst = {start, end};
+        return Rst;
+    }
+};
---
+++ 每日一题 6-8 月/Day0802_flatten-binary-tree-to-linked-list.cpp
@@ -0,0 +1,50 @@
+/*
+ * @Author      :vimday
+ * @Desc        :
+ * @Url         :
+ * @File Name   :Day0802_flatten-binary-tree-to-linked-list.cpp
+ * @Created Time:2020-08-02 00:36:18
+ * @E-mail      :lwftx@outlook.com
+ * @GitHub      :https://github.com/vimday
+ */
+#include <bits/stdc++.h>
+using namespace std;
+void debug() {
+#ifdef LOCAL
+    freopen("E:\\Cpp\\in.txt", "r", stdin);
+    freopen("E:\\Cpp\\out.txt", "w", stdout);
+#endif
+}
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if (!root) return;
+        flatten(root->left);
+        TreeNode *t = root->left, *lr = root;
+        while (t) {
+            lr = t;
+            t = t->right;
+        }
+        flatten(root->right);
+        TreeNode* r = root->right;
+        root->right = root->left;
+        root->left = nullptr;
+        lr->right = r;
+    }
+};
